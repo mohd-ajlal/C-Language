18fe83e977fc444680ac0d824cab1ef893509887
Mohd Ajlal <ajlal00786@gmail.com>
2023-02-01 02:59:18 +0530

    c

---
+++ Basic/variable.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+    int main(){
+    int a = 1;
+    char star = '*';
+    float decimal = 1.2;
+/*
+rules of variable:
+
+a. Variables are case sensitive
+b. First character is alphabet or '_'
+c. no commas or blank scace
+d. no matter symbol other than '_'
+*/
+    printf("%d\n", a);
+    printf("%c\n", star);
+    printf("%f\n", decimal);
+
+    return 0;
+}
