366442d2f19f86ee34ab18fb9e40c0a4151b8ef8
Mohd Ajlal <ajlal00786@gmail.com>
2023-02-02 00:57:06 +0530

    C

---
+++ Basic/comments.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+    int main(){
+    printf("This is a single line comment."); // This is a single line comment
+    printf("This is multiple line comment."); /* This is multi line comment */
+    return 0;
+}
---
+++ Basic/program_structure.c
@@ -0,0 +1,6 @@
+#include<stdio.h> // Preprocessor directive
+
+    int main(){
+    
+    return 0;
+}
