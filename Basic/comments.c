294c0fbddcaa29c2f9bfe724bdf1f27b532efb2c
Mohd Ajlal <ajlal00786@gmail.com>
2023-02-02 01:09:09 +0530

    C

--- Basic/comments.c
+++ Basic/comments.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
     int main(){
-    printf("This is a single line comment."); // This is a single line comment
-    printf("This is multiple line comment."); /* This is multi line comment */
+    printf("This is a single line comment."); 
+    // This is a single line comment
+    printf("This is multiple line comment."); 
+    /* This is
+     multi line
+      comment */
     return 0;
 }
---
+++ Basic/print_age.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+    int main(){
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Your age is %d", age);
+    return 0;
+}
