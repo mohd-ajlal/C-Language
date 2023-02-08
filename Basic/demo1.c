f46946fcd5045c55a9430403d39fa52bca272127
Mohd Ajlal <ajlal00786@gmail.com>
2023-02-08 14:27:04 +0530

    C

---
+++ Basic/demo1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+    int main(){
+    int a = 5, b = 6;
+    int sum = a +b;
+    printf("%d", sum);
+    return 0;
+}
---
+++ Basic/first_program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+    void main(){
+    printf("hello World");
+}
+
+// # preprocessing directive
+// #include<stdio.h> is a preprocessor directive that tells the compiler to include the contents of stdio.h file in the program.
+// #include is a preprocessor directive that tells the compiler to include the contents of the file stdio.h in the program.
+
+// stdio.h is a header file that contains the declarations of the standard input/output functions.
+// void means that the function main() does not return any value.
+// void main() is the main function of the program.
+// printf() is a function that prints the string "Hello World" on the screen.
