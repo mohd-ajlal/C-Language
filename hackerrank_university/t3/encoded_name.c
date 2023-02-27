12e2583f8e8f1516ad54551e601daae1f4808208
Mohd Ajlal <ajlal00786@gmail.com>
2023-02-27 22:07:45 +0530

    C

--- hackerrank_university/t3/encoded_name.c
+++ hackerrank_university/t3/encoded_name.c
@@ -32,17 +32,14 @@
 
 // ABCDE
 
-#include<stdio.h>
-#include<string.h>
-int main(){
-    char str[100];
-    char *token;
-    scanf("%[^\n]", str);
-    token = strtok(str, " ");
-    printf("%s", token);
-    while(token != NULL){
-        printf("%c", (int)token);
-        token = strtok(NULL, " ");
-    }
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() 
+{
+    int a,b,c,d,e;
+    scanf("%d&%d&%d&%d&%d",&a,&b,&c,&d,&e);
+    printf("%c%c%c%c%c",a,b,c,d,e);
     return 0;
 }
