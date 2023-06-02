e0e06ca30873f201e257632b7ff8b5227d1ab766
Mohd Ajlal <ajlal00786@gmail.com>
2023-06-03 01:03:15 +0530

    C LANGUAGE

--- DMA/malloc.c
+++ DMA/malloc.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
     int main(){
     int n, i;
-    void *p;
+    int *p;
     printf("Enter size: ");
     scanf("%d",&n);
     p = (int *)malloc(n*sizeof(int));
@@ -11,7 +11,7 @@
         scanf("%d", (p+i));
     }
     for(i = 0; i < n; i++){
-        printf("%d", *(p+i));
+        printf("%d ", *(p+i));
     }
     free(p);
     return 0.;
