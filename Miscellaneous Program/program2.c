ffc00caffd32b6cbf3a297fddd9fa5d65d459ea1
Mohd Ajlal <ajlal00786@gmail.com>
2023-07-14 03:01:36 +0530

    C Language

---
+++ Miscellaneous Program/program2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+    int a=4,b=5,c=a+b;
+    printf("sum of a=%d & b=%d is c=%d",a,b,c);
+}
---
+++ Miscellaneous Program/program3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a=10,b=2,c;
+    printf("sum of no. is %d\n",c=a+b);
+    printf("subtraction od no. is %d\n",c=a-b);
+    printf("multiply of no. is %d\n",c=a*b);
+    printf("divison od no. is %d\n",c=a/b);
+}
