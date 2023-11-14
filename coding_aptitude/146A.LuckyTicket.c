59e235744ad7731f73841232081add2d6b69907d
Mohd Ajlal <ajlal00786@gmail.com>
2023-11-15 03:17:40 +0530

    C

---
+++ coding_aptitude/146A.LuckyTicket.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+    int main(){
+    char num[101];
+    int n, first_sum, second_sum, length = 0;
+    scanf("%d", &n);
+    scanf("%s", &num);
+    for(int i =0; i<strlen(num); i++){
+        if(num[i] == '4' || nnum[i == '7']){
+            length++;
+        }
+        if(i<n/2){
+            first_sum = first_sum + int(num[i]);
+        }
+        if(i>=n/2){
+            second_sum = second_sum + int(num[i]);
+        }
+        if(num[i]!= )
+    }
+    if(first_sum == second_sum){
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+    return 0;
+}
